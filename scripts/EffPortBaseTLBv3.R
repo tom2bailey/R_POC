e1505100facb06d0a63e5f0bc18ead0cea8b5693
Tom Bailey <42701724+tom2bailey@users.noreply.github.com>
2018-12-23 21:51:19 -0500

    Interim check-in

--- scripts/EffPortBaseTLBv3.R
+++ scripts/EffPortBaseTLBv3.R
@@ -3,22 +3,140 @@
 #
 # original quadprog related coded from: http://economistatlarge.com/portfolio-theory/r-optimized-portfolio
 #
-# Functions and data records:
+# Primary functions and data records:
 #   effPort_MakeQuadParams: returns list of Amat, bvec, dvec, and meq parameters for quadprod solver from input parameters
 #   effPort_MakeQuadResultsMatrix: returns an empty matrix of the correct size, shape, and columns names for frontier results
 #   effPort_SinglePoint: returns results for a single point on the efficient frontier
-#   effPort_MakeFrontier: returns results for an entire frontier of points
-#   effPort_CalcRiskMult: returns risk multiplier to hit near a selected portfolio risk target
+##   effPort_MakeFrontier: returns results for an entire frontier of points
+##   effPort_CalcRiskMult: returns risk multiplier to hit near a selected portfolio risk target
 # add routines for:
-#   routines to sort expected returns and covariances into ascending or descending order of return, or std dev.
-#   cleanup results to only those tickers used in a portfolio
-#   plot base results
-#   standard data record to hold ticker inputs
-#   standard data record to hold frontier outputs, including notations
-#   standard data record to hold ticker inputs and any number of frontier outputs
-#   routines to create the normal set of plots from the above
+##   routines to sort expected returns and covariances into ascending or descending order of return, or std dev.
+##   cleanup results to only those tickers used in a portfolio
+##   plot base results
+##   standard data record to hold ticker inputs
+##   standard data record to hold frontier outputs, including notations
+##   standard data record to hold ticker inputs and any number of frontier outputs
+##   routines to create the normal set of plots from the above
+# Test functions and related stuff
+#
 
-effPort_MakeQuadResultsMatrix <- function(covRet, nPoints) {
+# required packages - to be loaded when we source this file
+# data.table - required for fread function and data.table structure, and related operations
+if (require(data.table) == FALSE) {
+  install.packages('data.table')
+  library(data.table)
+  }
+
+effport_NormalizeNSmooth <-
+  function(orig.dt,
+           nPtsSmooth,
+           nPtsReturnCalc,
+           nPtsCovarCalc) {
+    # Normalizes data for each ticker to the most recent price value of that ticker.
+    # Smooths data to remove high frequency variation (center filter to avoid phase / time shift)
+    #   NB: nPtsSmooth for filter must be an odd number for correct smoothing
+    # Performs calcuations for smoothing and calculated log-diff returns 
+    #   future: allow multiple types of returns to be calculated
+    # Returns a list of:
+    #   raw (input) data (data.table)
+    #   smoothed, augmented data (data.table), 
+    #   smoothing value (number of filter points),
+    #   type of return calculation (text;  "LogDiff", "ArithReturn", "ArithMultiplier")
+    
+    #check for nPtsSmooth to be an odd value
+    nSmooth <- as.integer(nPtsSmooth)
+    if ((nSmooth %% 2) == 0) {
+      stop('Error - nPtsSmooth must be odd for correct smoothing - please investigate.')
+    }
+    
+    # create normalized and smoothed values, normalized by most recent date value
+    norm.dt <- orig.dt[order(Ticker,-Date)]
+    norm.dt[, PriceNorm := Price / Price[1], by = Ticker]
+    norm.dt[, PriceNormSmooth := rollmean(PriceNorm, nSmooth, align = "center", fill =
+                                            NA), by = Ticker]
+    
+    # calculate log diff. 
+    # NB:  use a 'lead' shift as  the data is currently sorted in descending chronological order.
+    norm.dt[, RetLogDiff := log10(PriceNormSmooth) - log10(shift(PriceNormSmooth, 1, type =
+                                                                "lead")) , by = Ticker ]
+    ilist <- list(
+      dt.orig = orig.dt,
+      dt.smooth = norm.dt,
+      nPtsSmooth = nPtsSmooth,
+      Ret.Type = "LogDiff"
+    )
+    return(ilist)
+    
+  }
+
+
+effport_ReadCSV <- 
+  function(fileNm) {
+    #read CSV file of stacked adjusted daily closing price for any number of tickers
+    #expects and checks for 3 columns:
+      # Date - date as text, in "%m/%d/%Y" format that can be converted to a true date with as.Date()
+      # Ticker - text value of the asset or fund for this row
+      # Price - adjusted closing price for the ticker as of this date.
+    #Note: we assume adjustments are based on the most recent date in the file
+    #Note: all other columns in the CSV file are removed
+
+    #test if the requested file name is valid
+    if (file_test("-f", fileNm) == FALSE) { stop('fileNm not found - please investigate.')}
+
+    #read input data
+    dt <- fread( fileNm )
+    dt$Date <- as.Date(dt$Date, "%m/%d/%Y")
+    
+    #check for the required column names: Date, Ticker, Price
+    if (! all( c("Date", "Ticker", "Price") %in% names(dt) ) ) { 
+      stop('Required columms of Date, Ticker, and Price not found - please investigate.')
+    }
+    
+    #drop all other columns
+    dt <- dt[,c("Date", "Ticker", "Price")]
+    
+    return( dt )
+    }
+
+effPort_SinglePoint <-
+  function(expRet,
+           covRet,
+           QPparams,
+           risk.multiplier) {
+    #Output: single row matrix of results (per effPort_MakeQuadResultsMatrix)
+    #Inputs:
+    
+    #prep inputs and create storage for results
+    covRet <- as.matrix(covRet)
+    n <- ncol(covRet)
+    my.QRes <- effPort_MakeQuadResultsMatrix(covRet, 1)
+    
+    #adjust dvec (from qp params) for risk
+    my.dvec <- QPparams$dvec * risk.multiplier
+    
+    #add calc
+    iloop <- 1
+    my.sol <-
+      solve.QP(
+        covRet,
+        dvec = my.dvec,
+        Amat = QPparams$Amat,
+        bvec = QPparams$bvec,
+        meq = QPparams$meq
+      )
+    my.QRes[iloop, "Std.Dev"] <- sqrt(sum(my.sol$solution * colSums((covRet * my.sol$solution))))
+    my.QRes[iloop, "Exp.Ret"] <- as.numeric(my.sol$solution %*% expRet)
+    my.QRes[iloop, "Sharpe"] <- (my.QRes[iloop, "Exp.Ret"]) / my.QRes[iloop, "Std.Dev"]
+    my.QRes[iloop, "Idx"] <- iloop
+    my.QRes[iloop, 1:n] <- my.sol$solution
+    
+    #output results
+    return(my.QRes)
+  }
+
+effPort_MakeQuadResultsMatrix <- 
+  function(covRet, 
+           nPoints) {
   # Output: empty matrix of the size and shape to hold efficient frontier points.list of Amat, bvec, dvec, and meq for quadprod function calls
   # Input Args:
   # covRet - m x m matrix of covariances between the assets
@@ -32,11 +150,10 @@ effPort_MakeQuadResultsMatrix <- function(covRet, nPoints) {
   n <- ncol(covRet)
   
   # Initialize a results matrix to contain allocation and statistics
-  iMat <- matrix(nrow = loops, ncol = n + 4)
-  # Now give column names to the output matrix
-  colnames(iMat) <-
-    c(colnames(covRet), "Std.Dev", "Exp.Ret", "Sharpe", "Idx")
+  iMat <- matrix(nrow = nPoints, ncol = n + 4)
   
+  # Now give column names to the output matrix
+  colnames(iMat) <- c(colnames(covRet), "Std.Dev", "Exp.Ret", "Sharpe", "Idx")
   return(iMat)
 }
 
@@ -95,7 +212,13 @@ effPort_MakeQuadParams <-
   }
 
 
-makeEfficientFrontier <- function (expRet, covRet, short="no", max.allocation=NULL, risk.premium.up=.5, risk.increment=.005){
+makeEfficientFrontier <- 
+  function(expRet, 
+            covRet, 
+            short="no", 
+            max.allocation=NULL, 
+            risk.premium.up=.5, 
+            risk.increment=.005){
   # Input Args
   # expRet - m x 1 vector of expected returns - row order must match the covariance matrix row & column order
   # covRet - m x m matrix of covariances between the assets
